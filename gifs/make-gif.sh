b21bde816f4b654fc77552b9dd1f25c5354c9a9b
Ethan Bensman <ebenz99@gmail.com>
2020-02-14 21:30:45 -0500

    bg change

---
+++ gifs/make-gif.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# brew install x265
+# brew install ffmpeg
+# brew install youtube-dl
+# brew install imagemagick
+
+ID='BDjpPBLRMbA' # YouTube video ID, i.e. https://www.youtube.com/watch?v={ID}
+# convert the video file to GIF with ffmpeg
+START='00:00:58.640' # start 4 seconds in
+LENGTH='00:00:7.600' # end after 5 seconds
+SIZE='1000x562' # size of the output GIF
+
+
+# fetch the video file with youtube-dl
+# convert it to MP4 (not really needed, but keeps the filename predictable)
+# if [ ! -f $ID.mp4 ]; then
+#   youtube-dl --no-check-certificate --output '%(id)s.%(ext)s' --recode-video mp4 $ID
+# fi
+
+
+
+ffmpeg -i $(youtube-dl -f 18 --get-url https://www.youtube.com/watch?v=$ID) -ss $START -t $LENGTH -c:v copy -c:a copy $ID.mp4
+
+ffmpeg -ss $START -i $ID.mp4 -pix_fmt rgb8 -r 10 -s $SIZE -t $LENGTH $ID-unoptimized.gif
+# optimize the GIF with imagemagick
+convert -layers Optimize $ID-unoptimized.gif $ID.gif
+
+# credits:
+# http://www.commandlinefu.com/commands/view/10002/create-an-animated-gif-from-a-youtube-video
+# http://superuser.com/a/436109/106809
